3fd74ddcce2624d9008d3627afd25e72eddff44f
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-11-25 00:36:43 -0300

    :wastebasket: remove: Removendo arquivos não utilizados do projeto

--- script.js
+++
@@ -1,8 +0,0 @@
-const containerDark = document.querySelector('.container-dark');
-const buttonDark = document.querySelector('.button-dark');
-
-buttonDark.onclick = function() {
-    this.classList.toggle('active');
-    containerDark.classList.toggle('active');
-}
- 
